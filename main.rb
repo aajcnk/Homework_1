0e99629d391d3d0bac8b0c4e4a04b75b8127de8b
aajcnk <jacenko@mail.ua>
2016-02-07 09:24:03 +0200

    HW_2

--- main.rb
+++ main.rb
@@ -1,19 +1,17 @@
+x_point, y_point, x, y = ARGV
 
-x1, y1, x2, y2 = [10, 11, 17, 13]
-
-if x1 == x2 && y1 == y2
+if x_point == x and y_point == y
 	puts "Точка найдена!"
-else 
+elsif 
+	x_point != x and y_point != y
 	puts "Близко, но нет"
-end
-
-if 
-	x1 == x2 && y1 != y2
+elsif 
+	x_point == x and y_point != y
+	puts "Близко, но нет"
 	puts "х координата верна, y нет"
-
 elsif
-	x1 != x2 && y1 == y2
+	x_point != x and y_point == y
+	puts "Близко, но нет"
 	puts "y координата верна, x нет"
-else 
-	
-end
+else
+end 	
